5f4ec99e70c1fc6651f0df9205de403a3ccb01b7
Megan Kaluza <megan.kaluza@gmail.com>
2017-01-17 22:48:14 -0800

    add functioning modal to all images

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,73 @@
 $(document).ready(function(){
   $('#like').click(function(){
-    $('.ui.modal')
-  .modal({
-    blurring: true
-  })
-  .modal('show');
-    $('.columns').toggle('hide');
+    $('.one')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
   });
-  $('.close').click(function(){
-    $('.columns').toggle('show');
-  })
+
+  $('#likeTwo').click(function(){
+    $('.two')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+
+  $('#likeThree').click(function(){
+    $('.three')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+
+  $('#likeFour').click(function(){
+    $('.four')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+
+  $('#likeFive').click(function(){
+    $('.five')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+
+  $('#likeSix').click(function(){
+    $('.six')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+
+  $('#likeSeven').click(function(){
+    $('.seven')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+
+  $('#likeEight').click(function(){
+    $('.eight')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+
+  $('#likeNine').click(function(){
+    $('.nine')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
 });
