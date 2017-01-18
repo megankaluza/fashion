0914fd27e4691abde417378631cab136ace054d8
Megan Kaluza <megan.kaluza@gmail.com>
2017-01-17 21:42:19 -0800

    add modal using semantic/jquery/gulp

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   $('#like').click(function(){
-    $('.ui.modal').modal('show');
+    $('.ui.modal')
+  .modal({
+    blurring: true
+  })
+  .modal('show');
+    $('.columns').toggle('hide');
   });
+  $('.close').click(function(){
+    $('.columns').toggle('show');
+  })
 });
